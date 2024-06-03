cd8b1987c5eef82bbe202c2fe0cc9a140d248d3f
Ashari12 <rahmahdane412@gmail.com>
2024-06-04 02:43:09 +0700

    split data

--- src/main/java/com/newscategory/nlp/NewsCategory.java
+++ src/main/java/com/newscategory/nlp/NewsCategory.java
@@ -9,6 +9,7 @@ import java.util.List;
 import com.opencsv.CSVReader;
 
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import opennlp.tools.tokenize.SimpleTokenizer;
 
 public class NewsCategory {
     public static void main(String[] args) throws Exception {
@@ -44,8 +45,7 @@ public class NewsCategory {
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } 
-         catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -56,12 +56,28 @@ public class NewsCategory {
         // Print the dataset after text preprocessing
         printDataset(data);
 
+        // Convert data to required format
+        String[] texts = new String[data.size()];
+        int[] categoryIds = new int[data.size()];
+
+        for (int i = 0; i < data.size(); i++) {
+            texts[i] = data.get(i)[1];
+            categoryIds[i] = getCategoryId(data.get(i)[2]);
+        }
+
+        // Transform text to features
+        int maxFeatures = 5000;
+        int[][] x = transformTextToFeatures(texts, maxFeatures);
+
+        System.out.println("X.shape = " + x.length + " x " + x[0].length);
+        System.out.println("y.shape = " + categoryIds.length);
+
         // Split the dataset into training and test sets
-        DataSplitter.TrainTestData splitData = DataSplitter.splitData(data, 0.3, 0);
+        DataSplitter.DataSplitResult splitData = DataSplitter.splitData(x, categoryIds, 0.3, 0);
 
         // Obtain train-test data
-        String[] xTrain = splitData.xTrain;
-        String[] xTest = splitData.xTest;
+        int[][] xTrain = splitData.xTrain;
+        int[][] xTest = splitData.xTest;
 
         // Print sizes of training and test sets (optional)
         System.out.println("Train : " + xTrain.length);
@@ -100,5 +116,22 @@ public class NewsCategory {
             default:
                 return -1;
         }
+    }
+
+    private static int[][] transformTextToFeatures(String[] texts, int maxFeatures) {
+        SimpleTokenizer tokenizer = SimpleTokenizer.INSTANCE;
+        List<int[]> featuresList = new ArrayList<>();
+
+        for (String text : texts) {
+            String[] tokens = tokenizer.tokenize(text);
+            int[] features = new int[maxFeatures]; // Fixed size array
+
+            for (int i = 0; i < Math.min(tokens.length, maxFeatures); i++) {
+                features[i] = tokens[i].hashCode(); // Simple feature extraction using hash codes
+            }
+            featuresList.add(features);
+        }
+
+        return featuresList.toArray(new int[featuresList.size()][]);
     }
 }
